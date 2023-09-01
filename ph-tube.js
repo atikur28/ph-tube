c3219ae4cd3170e875bfddba42ae32e6ee113f1d
atikur28 <atikur45683@gmail.com>
2023-09-01 16:58:32 +0600

    Time Showing Completed

--- ph-tube.js
+++ ph-tube.js
@@ -22,52 +22,121 @@ const videoCard = async (categoryId) => {
     cardContainer.textContent = '';
     const cardContainer2 = document.getElementById('card-container2');
     cardContainer2.textContent = '';
+
     if(data.status === true){
         cardItems.forEach((item) =>{
             const verify = item.authors[0].verified;
+            // console.log(item);
+            const seconds = item.others.posted_date;
+            const second = parseInt(seconds);
+            const hourLeft = Math.floor(second / 3600);
+            const min = Math.floor((second - hourLeft * 3600) / 60);
+            const time = hourLeft + ' hours ' + min + ' min';
+            const number = 0;
+            // console.log(time);
             if(verify === true){
-              const div = document.createElement('div');
-            div.classList = `card w-fit bg-base-100 shadow-xl`;
-            div.innerHTML = `
-            <figure><img class="h-80 w-fit" src=${item.thumbnail} alt="Shoes" /></figure>
-            <div class="card-body flex flex-row items-start justify-around">
-              <div class="w-max">
-               <img class="rounded-full w-10 h-10" src=${item.authors[0].profile_picture} alt="">
-              </div>
-              <div>
-               <h2 class="card-title">${item.title}</h2>
-               <div class="flex items-center gap-1">
-                <p>${item.authors[0].profile_name}</p>
-                <div>
-                  <img src="./logos/verified.svg" alt="">
+              if(hourLeft > number && min > number){
+                const div = document.createElement('div');
+                div.classList = `card container mx-auto w-fit bg-base-100 shadow-xl`;
+                div.innerHTML = `
+                <figure><img class="relative h-80 w-96" src=${item.thumbnail} alt="Shoes" /></figure>
+                <div class="absolute right-3 bottom-36 bg-black w-max px-2 py-1 rounded-md">
+                  <p class="text-white text-xs">${time}</p>
                 </div>
-               </div>
-               <p>${item.others.views} views</p>
-              </div>
-            </div>
-            `;
-            cardContainer.appendChild(div);
+                <div class="card-body flex flex-row items-start justify-around">
+                  <div class="w-max">
+                   <img class="rounded-full w-10 h-10" src=${item.authors[0].profile_picture} alt="">
+                  </div>
+                  <div>
+                   <h2 class="card-title">${item.title}</h2>
+                   <div class="flex items-center gap-1">
+                    <p>${item.authors[0].profile_name}</p>
+                    <div>
+                      <img src="./logos/verified.svg" alt="">
+                    </div>
+                   </div>
+                   <p>${item.others.views} views</p>
+                  </div>
+                </div>
+                `;
+                cardContainer.appendChild(div);
+              }
+              else{
+                const div = document.createElement('div');
+                div.classList = `card container mx-auto w-fit bg-base-100 shadow-xl`;
+                div.innerHTML = `
+                <figure><img class="relative h-80 w-96" src=${item.thumbnail} alt="Shoes" /></figure>
+                <div class="absolute right-3 bottom-36 bg-black w-max px-2 py-1 rounded-md hidden">
+                  <p class="text-white text-xs">${time}</p>
+                </div>
+                <div class="card-body flex flex-row items-start justify-around">
+                  <div class="w-max">
+                   <img class="rounded-full w-10 h-10" src=${item.authors[0].profile_picture} alt="">
+                  </div>
+                  <div>
+                   <h2 class="card-title">${item.title}</h2>
+                   <div class="flex items-center gap-1">
+                    <p>${item.authors[0].profile_name}</p>
+                    <div>
+                      <img src="./logos/verified.svg" alt="">
+                    </div>
+                   </div>
+                   <p>${item.others.views} views</p>
+                  </div>
+                </div>
+                `;
+                cardContainer.appendChild(div);
+              }
             }
             else{
-              const div = document.createElement('div');
-            div.classList = `card w-fit bg-base-100 shadow-xl`;
-            div.innerHTML = `
-            <figure><img class="h-80 w-fit" src=${item.thumbnail} alt="Shoes" /></figure>
-            <div class="card-body flex flex-row items-start justify-around">
-              <div class="w-max">
-               <img class="rounded-full w-10 h-10" src=${item.authors[0].profile_picture} alt="">
+              if(hourLeft > number && min > number){
+                const div = document.createElement('div');
+              div.classList = `card container mx-auto w-fit bg-base-100 shadow-xl`;
+              div.innerHTML = `
+              <figure><img class="h-80 w-96" src=${item.thumbnail} alt="Shoes" /></figure>
+              <div class="absolute right-3 bottom-36 bg-black w-max px-2 py-1 rounded-md">
+                <p class="text-white text-xs">${time}</p>
               </div>
-              <div>
-               <h2 class="card-title">${item.title}</h2>
-               <div class="flex items-center gap-1">
-                <p>${item.authors[0].profile_name}</p>
-                <img src="" alt="">
-               </div>
-               <p>${item.others.views} views</p>
+              <div class="card-body flex flex-row items-start justify-around">
+                <div class="w-max">
+                 <img class="rounded-full w-10 h-10" src=${item.authors[0].profile_picture} alt="">
+                </div>
+                <div>
+                 <h2 class="card-title">${item.title}</h2>
+                 <div class="flex items-center gap-1">
+                  <p>${item.authors[0].profile_name}</p>
+                  <img src="" alt="">
+                 </div>
+                 <p>${item.others.views} views</p>
+                </div>
               </div>
-            </div>
-            `;
-            cardContainer.appendChild(div);
+              `;
+              cardContainer.appendChild(div);
+              }
+              else{
+                const div = document.createElement('div');
+              div.classList = `card container mx-auto w-fit bg-base-100 shadow-xl`;
+              div.innerHTML = `
+              <figure><img class="h-80 w-96" src=${item.thumbnail} alt="Shoes" /></figure>
+              <div class="absolute right-3 bottom-36 bg-black w-max px-2 py-1 rounded-md hidden">
+                <p class="text-white text-xs">${time}</p>
+              </div>
+              <div class="card-body flex flex-row items-start justify-around">
+                <div class="w-max">
+                 <img class="rounded-full w-10 h-10" src=${item.authors[0].profile_picture} alt="">
+                </div>
+                <div>
+                 <h2 class="card-title">${item.title}</h2>
+                 <div class="flex items-center gap-1">
+                  <p>${item.authors[0].profile_name}</p>
+                  <img src="" alt="">
+                 </div>
+                 <p>${item.others.views} views</p>
+                </div>
+              </div>
+              `;
+              cardContainer.appendChild(div);
+              }
             }
             // console.log(item);
         });
@@ -88,6 +157,7 @@ const videoCard = async (categoryId) => {
     sortByView(categoryId);
 }
 
+// Still solving
 const sortByView = async (categoryId) =>{
     const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`);
     const data = await response.json();
@@ -98,9 +168,7 @@ const sortByView = async (categoryId) =>{
       const views = item.others.views;
       const totalViews = parseFloat(views);
       const newViews = totalViews * 1000;
-      console.log(newViews);
-      // const div = document.createElement('div');
-      // div.innerHTML = 
+      // console.log(item);
     });
 }
 
